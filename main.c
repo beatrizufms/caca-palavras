34db33924ec847672f19acee98f84b87d6e6ae7c
Beatriz <89365231+beatrizufms@users.noreply.github.com>
2021-09-04 19:07:42 -0400

    Update main.c

--- main.c
+++ main.c
@@ -65,16 +65,33 @@ void marcar_ocorrencia_vertical(char palavra[],
         char A[MAX_ALTURA][MAX_LARGURA],
         char B[MAX_ALTURA][MAX_LARGURA],
         int m, int n, int i, int j) {
-  /* Fazer */
+  int k = 0;
+  while (j+k < m && A[i+k][j] == palavra[k] && palavra[k] != '\0')
+    k++;
+  if (palavra[k] == '\0') /* achamos a palavra */
+    for (k--; k >= 0; k--)
+      B[i+k][j] = palavra[k];
 }
 
 void marcar_ocorrencia_diagonal(char palavra[],
         char A[MAX_ALTURA][MAX_LARGURA],
         char B[MAX_ALTURA][MAX_LARGURA],
         int m, int n, int i, int j) {
-  /* Fazer: */
+  
   /* Diagonal para direita e para baixo */
+  int k = 0;
+  while (i+k < m && j+k < n && A[i+k][j+k] == palavra[k] && palavra[k] != '\0')
+    k++;
+  if (palavra[k] == '\0') /* achamos a palavra */
+    for (k--; k >= 0; k--)
+      B[i+k][j+k] = palavra[k];
   /* Diagonal para esquerda e para baixo */
+  int k = 0;
+  while (i+k < m && j-k >= 0 && A[i+k][j-k] == palavra[k] && palavra[k] != '\0')
+    k++;
+  if (palavra[k] == '\0') /* achamos a palavra */
+    for (k--; k >= 0; k--)
+      B[i+k][j-k] = palavra[k];
 }
 
 void marcar_ocorrencias(char palavra[],
